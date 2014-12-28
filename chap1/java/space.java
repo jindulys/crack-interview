a84805550e4907e6cbaad2559f975910f1487d86
yansong li <857367901@qq.com>
2014-12-28 14:11:23 -0500

    question1.4

---
+++ chap1/java/space.java
@@ -0,0 +1,21 @@
+
+import java.io.*;
+
+public class space{
+
+  private static String replace(char[] ){
+    int strL = s.length();
+    new char
+  }
+
+  public static void main(String[] args) throws Exception{
+    InputStreamReader r = new InputStreamReader(System.in);
+    BufferedReader br = new BufferedReader(r);
+    String input = "";
+    while(!input.equals("stop")){
+      System.out.println("Please input a string:");
+      input = br.readLine();
+      System.out.println("Output : "+ input.length());
+    }
+  }
+}
