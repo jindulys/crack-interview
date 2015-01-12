955b6567e9f7d2dfddf18152d99e7a923cd44d08
yansong li <857367901@qq.com>
2015-01-12 12:18:57 -0500

    add3.4

---
+++ chap3_Stack_Queue/Tower.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Tower {
+    // The index of tower;
+    int index;
+
+    // Store the disks
+    Stack<Integer> disks;
+
+    public Tower(int i) {
+        index = i;
+        disks = new Stack<Integer>();
+    }
+
+    public void addTop(int i) throws Exception {
+        if (!disks.empty() && disks.peek() < i) throw new Exception("Add Top Not allowed");
+        disks.push(i);
+    }
+
+    public void moveTop(Tower des) throws Exception {
+        try {
+            int top = disks.pop();
+            des.addTop(top);
+            System.out.println("Moving" + top + "From Tower -" + this.index + "To Tower -" + des.index);
+        } catch (EmptyStackException e) {
+            throw new Exception("Move empty tower");
+        }
+    }
+
+    public void moveDisks(int n,Tower des, Tower buf) throws Exception{
+        if (n == 0) return;
+        moveDisks(n-1, buf, des);
+        moveTop(des);
+        buf.moveDisks(n-1,des,this);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Tower[] towers = new Tower[3];
+        for(int i = 0; i < 3; i++) {
+            towers[i] = new Tower(i);
+        }
+
+        for(int i = 6; i>0; i--) {
+            towers[0].addTop(i);
+        }
+
+        towers[0].moveDisks(2, towers[2], towers[1]);
+    }
+}
